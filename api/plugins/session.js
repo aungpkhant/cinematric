807def20b7891ab40d99effd5c575f9696e46197
aungpkhant <derrick.aungkhant@gmail.com>
2021-10-20 03:44:27 +0630

    🐛 Fix cookie domain prod

--- api/plugins/session.js
+++ api/plugins/session.js
@@ -23,6 +23,8 @@ const plugin = async function (fastify, opts) {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      domain:
+        process.env.NODE_ENV === "development" ? "localhost" : "cinematric.xyz",
     },
   });
 };
